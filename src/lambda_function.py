7d71c09e053c28e7a2d4572839cde812a29795f9
David Moreno <damog@damog.net>
2019-02-17 13:42:07 +0100

    using tweetpony instead

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,31 +6,28 @@ from datetime import datetime, date, time
 import secrets
 import os
 
-#sys.path.insert(0, 'deps')
-import twitter
-
+import tweetpony
 
 def lambda_handler(event = None, context = None):
     # TODO: UTC lol
+    start   = datetime(2001, 1, 1, 0, 0, 0)
+    end     = datetime(2100, 12, 31, 23, 59, 59)
 
-    start = datetime(2001, 1, 1, 0, 0, 0)
-    end = datetime(2100, 12, 31, 23, 59, 59)
-
-    full = end - start
+    full    = end - start
     elapsed = ( (datetime.now() - start).total_seconds() / full.total_seconds() ) * 100
 
-    on = ( 15 * elapsed ) / 100
-    off = 15 - on
-    bar_on = u'\u2593' * int(on)
+    on      = ( 15 * elapsed ) / 100
+    off     = 15 - on
+    bar_on  = u'\u2593' * int(on)
     bar_off = u'\u2591' * int(off)
-    text = '[' + bar_on + bar_off + ']' + ' ' + "%.4f" % elapsed + '%'
+    text    = bar_on + bar_off + ' ' + "%.4f" % elapsed + '%'
 
     if "TWEET" in os.environ:
-        api = twitter.Api(consumer_key=secrets.consumer_key,
-                          consumer_secret=secrets.consumer_secret,
-                          access_token_key=secrets.access_token_key,
-                          access_token_secret=secrets.access_token_secret)
-        status = api.PostUpdate( text )
+        api = tweetpony.API(    consumer_key        = secrets.consumer_key,
+                                consumer_secret     = secrets.consumer_secret,
+                                access_token        = secrets.access_token_key,
+                                access_token_secret = secrets.access_token_secret)
+        api.update_status(text)
 
     return { 'message' : text }
 
---
+++ src/secrets.py
@@ -0,0 +1,8 @@
+import os
+
+# Please do not steal :)
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token_key = os.environ.get('ACCESS_TOKEN_KEY')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+
